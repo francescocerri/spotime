05abe968f400d2ce9b05cc8d82dc9b29fd07893e
francescocerri <francesco.west96@gmail.com>
2020-08-30 12:20:43 +0200

    edit env script

--- internals/scripts/create-env.js
+++ internals/scripts/create-env.js
@@ -1,2 +1,19 @@
 const fs = require('fs');
-fs.writeFileSync('./.env', `CLIENT_ID=${process.env.CLIENT_ID}\nCLIENT_SECRET=${process.env.CLIENT_SECRET}`)
+
+console.log(process.env.CLIENT_ID);
+console.log(process.env.CLIENT_SECRET);
+const path = './.env';
+const data = `CLIENT_ID=${process.env.CLIENT_ID}\nCLIENT_SECRET=${process.env.CLIENT_SECRET}`;
+
+fs.open(path,'r',function(err, fd){
+  if (err) {
+    fs.writeFile(path, data, function(err) {
+      if(err) {
+        console.log(err);
+      }
+      console.log("The file was saved!");
+    });
+  } else {
+    console.log("The file exists!");
+  }
+});
